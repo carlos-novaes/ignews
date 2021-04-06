81dcba2d3729b7fd31b2812ebcd10b4a890c7637
Carlos Novaes <carlos.novaes@b2wdigital.com>
2021-04-05 21:31:31 -0300

    removendo coments

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
 export default NextAuth({
+  //
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
